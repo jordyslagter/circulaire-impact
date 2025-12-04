2345f1d572139a998e15c1cbdad0ca5bbed58608
Jordy Slagter <jordyslagter@icloud.com>
2025-12-04 21:33:26 +0100

    add new landing

---
+++ pitchdeck/tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    colors: {
+      greentomgreen: "#85af99",
+      greentomlightgreen: "#c2d7cc",
+      primary: "#030303",
+      antiprimary: "#f2f2f2",
+    },
+  },
+};
