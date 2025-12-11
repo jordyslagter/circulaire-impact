28c29c4d3df4fbab82b02f27b6323a646e927012
Jordy Slagter <jordyslagter@icloud.com>
2025-12-11 11:23:34 +0100

    add scroll trigger to listing

--- pitchdeck/src/components/sections/Listing.tsx
+++ pitchdeck/src/components/sections/Listing.tsx
@@ -1,19 +1,50 @@
+import { useGSAP } from "@gsap/react";
 import { clsx } from "clsx";
-import { FC, ReactElement } from "react";
+import gsap from "gsap";
+import { FC, ReactElement, useRef } from "react";
 import { IoChatbubblesOutline, IoPeopleCircleOutline } from "react-icons/io5";
 import { Section } from "../Section";
 
 interface ParagraphCellProps {
   children?: ReactElement | string;
+  [x: string]: any;
 }
 
-const ParagraphCell: FC<ParagraphCellProps> = ({ children }) => {
-  return <p className={clsx("w-[90%] max-w-80 text-center")}>{children}</p>;
+const ParagraphCell: FC<ParagraphCellProps> = (props) => {
+  const { children } = props;
+
+  return (
+    <p {...props} className={clsx("w-[90%] max-w-80 text-center")}>
+      {children}
+    </p>
+  );
 };
 
 export const Listing: FC = () => {
+  const container = useRef(null);
+  const paragraph1 = useRef(null);
+  const paragraph2 = useRef(null);
+
+  useGSAP(
+    () => {
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top 80%",
+          once: true,
+        },
+      });
+      timeline.fromTo(paragraph1.current, { opacity: 0 }, { opacity: 1 });
+      timeline.fromTo(paragraph2.current, { opacity: 0 }, { opacity: 1 });
+    },
+    { scope: container },
+  );
+
   return (
-    <Section className={clsx("grid grid-cols-2 grid-rows-2 items-center")}>
+    <Section
+      ref={container}
+      className={clsx("grid grid-cols-2 grid-rows-2 items-center")}
+    >
       <div
         className={clsx(
           "flex w-full flex-col content-center items-center justify-center",
@@ -29,7 +60,7 @@ export const Listing: FC = () => {
           "flex flex-col content-center items-center justify-center",
         )}
       >
-        <ParagraphCell>
+        <ParagraphCell ref={paragraph1}>
           Greentom heeft problemen met naamsbekendheid. Het bedrijf heeft het B
           Corp-certificaat behaald maar is nog altijd nauwelijks bekend in
           Nederland.
@@ -51,7 +82,7 @@ export const Listing: FC = () => {
           "bg-gtlightgreen h-full w-full",
         )}
       >
-        <ParagraphCell>
+        <ParagraphCell ref={paragraph2}>
           Het bedrijf heeft zich tot nu toe vooral gefocust op nieuwe ouders.
           Met ons idee om in te zetten op een studentenrugzak zal Greentom met
           een hele nieuwe doelgroep moeten werken.
