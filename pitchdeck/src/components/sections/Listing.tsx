2710b80b55734987cf84f11d98f2c572b5db79c3
Jordy Slagter <jordyslagter@icloud.com>
2025-12-11 11:12:30 +0100

    split paragraphs into their own component

--- pitchdeck/src/components/sections/Listing.tsx
+++ pitchdeck/src/components/sections/Listing.tsx
@@ -1,8 +1,17 @@
 import { clsx } from "clsx";
+import { FC, ReactElement } from "react";
 import { IoChatbubblesOutline, IoPeopleCircleOutline } from "react-icons/io5";
 import { Section } from "../Section";
 
-export const Listing = () => {
+interface ParagraphCellProps {
+  children?: ReactElement | string;
+}
+
+const ParagraphCell: FC<ParagraphCellProps> = ({ children }) => {
+  return <p className={clsx("w-[90%] max-w-80 text-center")}>{children}</p>;
+};
+
+export const Listing: FC = () => {
   return (
     <Section className={clsx("grid grid-cols-2 grid-rows-2 items-center")}>
       <div
@@ -14,17 +23,19 @@ export const Listing = () => {
         <IoChatbubblesOutline size={90} />
         <h1 className={clsx("mt-5 text-center text-4xl")}>Naamsbekendheid</h1>
       </div>
+
       <div
         className={clsx(
           "flex flex-col content-center items-center justify-center",
         )}
       >
-        <p className={clsx("w-[90%] max-w-80 text-center")}>
+        <ParagraphCell>
           Greentom heeft problemen met naamsbekendheid. Het bedrijf heeft het B
           Corp-certificaat behaald maar is nog altijd nauwelijks bekend in
           Nederland.
-        </p>
+        </ParagraphCell>
       </div>
+
       <div
         className={clsx(
           "flex flex-col content-center items-center justify-center",
@@ -33,17 +44,18 @@ export const Listing = () => {
         <IoPeopleCircleOutline size={90} />
         <h1 className={clsx("mt-5 text-center text-4xl")}>Nieuwe doelgroep</h1>
       </div>
+
       <div
         className={clsx(
           "flex flex-col content-center items-center justify-center",
           "bg-gtlightgreen h-full w-full",
         )}
       >
-        <p className={clsx("w-[90%] max-w-80 text-center")}>
+        <ParagraphCell>
           Het bedrijf heeft zich tot nu toe vooral gefocust op nieuwe ouders.
           Met ons idee om in te zetten op een studentenrugzak zal Greentom met
           een hele nieuwe doelgroep moeten werken.
-        </p>
+        </ParagraphCell>
       </div>
     </Section>
   );
