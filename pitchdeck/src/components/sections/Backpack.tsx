a4b5dc6db23d9caabcadf8b9fca3e8f8c1228780
Jordy Slagter <jordyslagter@icloud.com>
2025-12-11 10:29:56 +0100

    add animation to backpack text

--- pitchdeck/src/components/sections/Backpack.tsx
+++ pitchdeck/src/components/sections/Backpack.tsx
@@ -6,10 +6,40 @@ import { BsBackpack3 } from "react-icons/bs";
 import Section from "../Section";
 
 const Backpack: FC = () => {
-  const backpackText = useRef(null);
+  const backpackText1 = useRef(null);
+  const backpackText2 = useRef(null);
+  const backpackText3 = useRef(null);
+  const backpackText4 = useRef(null);
+  const backpackText5 = useRef(null);
 
   useGSAP(() => {
-    gsap.fromTo(backpackText.current, { y: 30 }, { y: 0 });
+    const timeline = gsap.timeline();
+
+    timeline.fromTo(
+      backpackText1.current,
+      { opacity: 0, y: 30 },
+      { opacity: 1, y: 0 },
+    );
+    timeline.fromTo(
+      backpackText2.current,
+      { opacity: 0, y: 30 },
+      { opacity: 1, y: 0 },
+    );
+    timeline.fromTo(
+      backpackText3.current,
+      { opacity: 0, y: 30 },
+      { opacity: 1, y: 0 },
+    );
+    timeline.fromTo(
+      backpackText4.current,
+      { opacity: 0, y: 30 },
+      { opacity: 1, y: 0 },
+    );
+    timeline.fromTo(
+      backpackText5.current,
+      { opacity: 0, y: 30 },
+      { opacity: 1, y: 0 },
+    );
   });
 
   return (
@@ -19,9 +49,13 @@ const Backpack: FC = () => {
       )}
     >
       <BsBackpack3 size={200} />
-      <p ref={backpackText} className={clsx("mt-5 text-4xl")}>
-        Zwaar, oncomfortabel, snel kapot, duur, saai
-      </p>
+      <div className={clsx("mt-5 flex flex-row gap-7 text-center text-4xl")}>
+        <p ref={backpackText1}>Zwaar</p>
+        <p ref={backpackText2}>oncomfortabel</p>
+        <p ref={backpackText3}>snel kapot</p>
+        <p ref={backpackText4}>duur</p>
+        <p ref={backpackText5}>saai</p>
+      </div>
     </Section>
   );
 };
