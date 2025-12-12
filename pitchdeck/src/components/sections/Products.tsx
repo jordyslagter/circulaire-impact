03f20adb18ff3771bc4cf0176bedf1af8aa95fc2
Jordy Slagter <jordyslagter@icloud.com>
2025-12-12 12:42:21 +0100

    remake products section

--- pitchdeck/src/components/sections/Products.tsx
+++ pitchdeck/src/components/sections/Products.tsx
@@ -1,68 +1,95 @@
 import { useGSAP } from "@gsap/react";
 import { clsx } from "clsx";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import Image from "next/image";
 import { FC, useRef } from "react";
 import { Section } from "../Section";
 
-export const Products: FC = () => {
+interface ProductCardProps {
+  title: string;
+  description: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  [x: string]: any;
+}
+
+const ProductCard: FC<ProductCardProps> = (props) => {
+  const { title, description, image, ...rest } = props;
+
   const container = useRef(null);
-  const row1col1 = useRef(null);
-  const row1col2 = useRef(null);
-  const row2col1 = useRef(null);
-  const row2col2 = useRef(null);
 
   useGSAP(
     () => {
       const timeline = gsap.timeline({
-        scrollTrigger: container.current,
-        start: "top 80%",
-        once: true,
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top 80%",
+          once: true,
+        },
       });
 
-      const from = { opacity: 0, y: 10 } as GSAPTweenVars;
-      const to = { opacity: 1, y: 0 } as GSAPTweenVars;
-
-      timeline.fromTo(row1col1.current, from, to);
-      timeline.fromTo(row1col2.current, from, to);
-      timeline.fromTo(row2col1.current, from, to);
-      timeline.fromTo(row2col2.current, from, to);
+      timeline.fromTo(
+        container.current,
+        { opacity: 0, y: 10 },
+        { opacity: 1, y: 0 },
+      );
     },
     { scope: container },
   );
 
   return (
+    <div ref={container} className={clsx("h-full w-full p-5")}>
+      <article
+        {...rest}
+        className={clsx(
+          "grid h-full w-full grid-cols-2 grid-rows-2 rounded border p-5",
+          "content-center items-center justify-center justify-items-center",
+          "shadow transition hover:shadow-xl",
+        )}
+      >
+        <Image src={image.src} alt={image.alt} width={140} height={140} />
+        <h1 className={clsx("text-xl font-bold")}>{title}</h1>
+        <p className={clsx("col-span-2")}>{description}</p>
+      </article>
+    </div>
+  );
+};
+
+export const Products: FC = () => {
+  return (
     <Section
-      ref={container}
       className={clsx(
-        "grid grid-cols-2 grid-rows-3 content-center items-center",
+        "grid grid-cols-2 grid-rows-2 content-center items-center",
         "justify-center justify-items-center text-center",
       )}
     >
-      <h1
+      <div
         className={clsx(
-          "bg-gtlightgreen col-span-2 h-full w-full text-4xl",
-          "flex flex-col content-center items-center justify-center font-bold",
+          "col-span-2 flex h-full w-full flex-col content-center items-center",
+          "bg-gtlightgreen justify-center justify-items-center",
         )}
       >
-        Huidige producten
-      </h1>
-      <Image
-        ref={row1col1}
-        src="https://greentom.com/media/cms/sdd/classic-2017-perspective-grey-blue.png"
-        width={250}
-        height={250}
-        alt="Stroller"
+        <h1 className={clsx("text-4xl font-bold")}>Huidige producten</h1>
+        <p className={clsx("mt-2 text-xs")}>Greentom (2025)</p>
+      </div>
+      <ProductCard
+        image={{
+          src: "https://greentom.com/media/cms/sdd/classic-2017-perspective-grey-blue.png",
+          alt: "Stroller",
+        }}
+        title="Stroller"
+        description="Stroller"
       />
-      <p ref={row1col2}>Stroller</p>
-      <Image
-        ref={row2col1}
-        src="https://greentom.com/media/catalog/product/cache/249688b60bb56e1686647a2df7a46765/g/t/gtbc1-sage.png"
-        width={250}
-        height={250}
-        alt="Draagzak"
+      <ProductCard
+        image={{
+          src: "https://greentom.com/media/catalog/product/cache/249688b60bb56e1686647a2df7a46765/g/t/gtbc1-sage.png",
+          alt: "Draagzak",
+        }}
+        title="Draagzak"
+        description="Draagzak"
       />
-      <p ref={row2col2}>Draagzak</p>
     </Section>
   );
 };
