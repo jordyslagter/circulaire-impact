db4efb950bf42a565103517a5a3c27374e118974
Jordy Slagter <jordyslagter@icloud.com>
2026-01-08 11:27:25 +0100

    add future

--- pitchdeck/src/components/sections/Future.tsx
+++ pitchdeck/src/components/sections/Future.tsx
@@ -1,9 +1,33 @@
+import { clsx } from "clsx";
+import { BsStars } from "react-icons/bs";
 import { Section } from "../Section";
 
 export const Future = () => {
   return (
-    <Section>
-      <p>Hello, World!</p>
+    <Section
+      className={clsx(
+        "flex flex-col content-center items-center justify-center",
+        "justify-items-center",
+      )}
+    >
+      <h1 className={clsx("text-4xl")}>De toekomst</h1>
+
+      <BsStars size={200} className={clsx("text-gtgreen mt-12")} />
+
+      <div className={clsx("flex max-w-200 flex-col gap-5 p-12")}>
+        <p>
+          Stel je voor dat een rugzak geen eindpunt meer is, maar een beginpunt:
+          een flexibel, modulair systeem dat met elke gebruiker meebeweegt en
+          nooit echt ‘af’ is. Een product dat niet gekozen wordt om hoe het
+          eruitziet bij aankoop, maar om hoe het zich blijft aanpassen in de
+          jaren daarna.
+        </p>
+
+        <p className={clsx("italic")}>
+          Een modulair ontwerp maakt één rugzak geschikt voor vele levens, en
+          dat is precies waar circulariteit begint.
+        </p>
+      </div>
     </Section>
   );
 };
